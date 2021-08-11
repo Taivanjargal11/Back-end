aaabf71d9e06bb55805888303b385670db5e1443
Taivanjargal11 <s18ud1101039@nmit.edu.mn>
2021-08-11 15:11:27 +0800

    First

---
+++ src/main/java/com/example/legalmongolia/LegalMongoliaApplication.java
@@ -0,0 +1,13 @@
+package com.example.legalmongolia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalMongoliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalMongoliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/legalmongolia/LegalMongoliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.legalmongolia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalMongoliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
